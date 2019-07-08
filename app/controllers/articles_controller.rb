423c12b7e9f8db5c14479516274c4fe890e8adc3
smaple with article <rathodpratik557@gmail.com>
2019-07-08 11:38:42 +0530

    all cahnges before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-    before_action :set_user, only: [:edit, :update, :show, :destroy]
-    
-    http_basic_authenticate_with name: "111", password: "111", except: [:index, :show]    
+    before_action :require_user, except: [:index,:show]
+    before_action :require_same_user, only: [:edit, :update, :destroy] 
+    http_basic_authenticate_with name: "111", password: "111", except: [:index, :show]  
+
 #new
     def new
         @article=Article.new
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
 #create
     def create
         @article = Article.new(article_params)
-        @article.user=User.first 
+        @article.user=current_user 
        if @article.save
         redirect_to @article
        else
@@ -18,6 +19,7 @@ class ArticlesController < ApplicationController
       end
 #show 
       def show
+        @article = Article.find(params[:id])
       end
 #index
       def index
@@ -25,9 +27,12 @@ class ArticlesController < ApplicationController
       end
 #edit
       def edit
+        @article=Article.find(params[:id])
       end
 #update
-      def update       
+      def update
+        @article = Article.find(params[:id])
+       
         if @article.update(article_params)
           redirect_to @article
         else
@@ -36,7 +41,9 @@ class ArticlesController < ApplicationController
       end
  
       def destroy
+        @article= Article.find(params[:id])
         @article.destroy
+
         redirect_to articles_path
       end
 
@@ -44,7 +51,12 @@ class ArticlesController < ApplicationController
         def article_params
             params.require(:article).permit(:title,:text)
         end
-        def set_user
-          @user=User.find(params[:id])
+        def require_same_user
+          if current_user != @article.user
+            flash[:danger]= "you can only edit and delete own articles"
+            redirect_to root_path
+
+        end
       end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    redirect_to articles_path if logged_in?
+  end
+
+  def home
+    
   end
 end
