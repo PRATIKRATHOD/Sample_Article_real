6ea1bb93a218f007d895e243d08e987ab17052e9
smaple with article <rathodpratik557@gmail.com>
2019-07-09 14:24:44 +0530

    changes

---
+++ test/integration/create_user_test.rb
@@ -0,0 +1,11 @@
+# require 'test_helper'
+
+# class CreateUserTest < ActionDispatch::IntegrationTest
+
+#     test "get signup from create user" do
+#         get signup_path
+#         assert_template 'user/new'
+#         assert_difference "User.count", 1 do
+#             post users_path
+
+# end
