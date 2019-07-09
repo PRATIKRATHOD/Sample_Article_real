56845ca8256fbcaa057dc4b0f8b962db62babcc7
smaple with article <rathodpratik557@gmail.com>
2019-07-09 11:36:00 +0530

    many to many association Article_category table

--- test/controllers/categories_controller.rb
+++ test/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     def setup
         @category= Category.create(name: "sports")
+        @user=User.create(username: "zzz", email: "zz@zz.com", password: "zzz", admin: true)
 
     test "should get category index" do
         get :categories_path
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should get new" do
+        sign_in_as(@user,"zzz")
         get :new_category_path
         assert_response :success
     end
@@ -20,5 +22,11 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
     end
 
-
+    test "should redirect create when admin not logged in" do
+        assert_no_difference 'Category.count' do
+        post :create, category: { name: "sports" }
+    end    
+        assert_redirected_to categories_path 
+        end_redirected_to categories_path
+    end
 end
